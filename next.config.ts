6c721c9d28d2fb8ed7a0be3356afc6100b6b8d05
Ridvan <ridvanpektas1990@gmail.com>
2025-10-03 21:21:39 +0200

    build: ignore eslint during builds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: { ignoreDuringBuilds: true },
+  // typescript: { ignoreBuildErrors: true }, // nur aktivieren, wenn irgendwann TS selbst blockt
 };
 
 export default nextConfig;
